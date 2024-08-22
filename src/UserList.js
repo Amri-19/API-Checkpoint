93471026718a762541e63a3b7d2334c96a964912
AMRI-21 <amriadam298@gmail.com>
2024-08-22 15:37:04 +0100

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function UserList() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setListOfUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {listOfUsers.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
